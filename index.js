ef3f977315528a898486427fd191fc60dd0574cc
Dat Vu <tuandatk25a@gmail.com>
2017-05-23 10:52:45 +0700

    second commit

--- index.js
+++ index.js
@@ -7,6 +7,67 @@ app.get('/', function(req, res){
     res.sendFile('./index.html', {root: __dirname});
 });
 
+var STREAM_SECRET = 12345,
+	STREAM_PORT = 8082,
+	WEBSOCKET_PORT = 8084,
+	STREAM_MAGIC_BYTES = 'jsmp'; 
+
+var width = 320,
+	height = 240;
+
+// Websocket Server
+var socketServer = new (require('ws').Server)({port: WEBSOCKET_PORT});
+socketServer.on('connection', function(socket) {
+	// Send magic bytes and video size to the newly connected socket
+	var streamHeader = new Buffer(8);
+	streamHeader.write(STREAM_MAGIC_BYTES);
+	streamHeader.writeUInt16BE(width, 4);
+	streamHeader.writeUInt16BE(height, 6);
+	socket.send(streamHeader, {binary:true});
+
+	console.log( 'New WebSocket Connection ('+socketServer.clients.length+' total)' );
+	
+	socket.on('close', function(code, message){
+		console.log( 'Disconnected WebSocket ('+socketServer.clients.length+' total)' );
+	});
+});
+
+socketServer.broadcast = function(data, opts) {
+	for( var i in this.clients ) {
+		if (this.clients[i].readyState == 1) {
+			this.clients[i].send(data, opts);
+		}
+		else {
+			console.log( 'Error: Client ('+i+') not connected.' );
+		}
+	}
+};
+
+
+// HTTP Server to accept incomming MPEG Stream
+var streamServer = require('http').createServer( function(request, response) {
+	var params = request.url.substr(1).split('/');
+
+	if( params[0] == STREAM_SECRET ) {
+		response.connection.setTimeout(0);
+		
+		width = (params[1] || 320)|0;
+		height = (params[2] || 240)|0;
+		
+		request.on('data', function(data){
+			socketServer.broadcast(data, {binary:true});
+		});
+	}
+	else {
+		console.log(
+			'Failed Stream Connection: '+ request.socket.remoteAddress + 
+			request.socket.remotePort + ' - wrong secret.'
+		);
+		response.end();
+	}
+}).listen(STREAM_PORT);
+
+
 app.listen(3000, function(){
     console.log('App listening on port 3000!')
 });
